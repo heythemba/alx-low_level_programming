394c6edc35565eb23b5ea56242bedddc57fe3056
Haythem <heythem.94@gmail.com>
2023-09-09 00:54:49 +0000

    Print alphabet

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 
 /**
-*Main - Prints the last digit of a randomly generated number and find whether it is greater than 5, less than 6, or 0.
-*
+*Main - Prints the last digit of a randomly generated number 
+*and find whether it is greater than 5, less than 6, or 0.
 * Return: Always 0.
 */
 int main(void)
