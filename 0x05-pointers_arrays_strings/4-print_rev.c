919abc9c74e729ba4248015d8dadf867f8b42d43
Haythem <heythem.94@gmail.com>
2023-09-18 21:01:10 +0000

    fix error

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * * print_rev - imprime en reversa
- * * @s: string
- * * Return: 0
- * */
+ * print_rev - imprime en reversa
+ * @s: string
+ * Return: 0
+ */
 
 void print_rev(char *s)
 {
