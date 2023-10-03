23a260d942423460a5fe1d120119fd3d3c7b1064
Haythem <heythem.94@gmail.com>
2023-10-03 00:02:22 +0000

    0x0A. C - argc, argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - entry point of the code
+ *@argc:argc
+ *@argv:argv
+ * Return: Always 0 (Success)
+ */
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
