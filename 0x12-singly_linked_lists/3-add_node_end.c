98e2b9f85497178136ef214abb7ef413f1b4de7f
Haythem <heythem.94@gmail.com>
2023-10-20 01:54:06 +0000

    replacing s with str

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -3,26 +3,26 @@
 /**
  * add_node_end - adds a node to the end of the list
  * @head: address of pointer to head node
- * @s: str field of node
+ * @str: str field of node
  *
  * Return: size of list
  */
-list_t *add_node_end(list_t **head, const char *s)
+list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node = malloc(sizeof(list_t));
 	list_t *node = *head;
 
 	if (!head || !new_node)
 		return (NULL);
-	if (s)
+	if (str)
 	{
-		new_node->s = strdup(s);
-		if (!new_node->s)
+		new_node->str = strdup(str);
+		if (!new_node->str)
 		{
 			free(new_node);
 			return (NULL);
 		}
-		new_node->len = _strlen(new_node->s);
+		new_node->len = _strlen(new_node->str);
 	}
 	if (node)
 	{
