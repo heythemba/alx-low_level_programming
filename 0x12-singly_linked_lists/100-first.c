433895d0c238e2c92e822156cf5a2a0eadd19805
Haythem <heythem.94@gmail.com>
2023-10-20 01:58:12 +0000

    fixing main function error

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -11,14 +11,3 @@ void __attribute__ ((constructor)) pre_main_function(void)
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
-
-/**
- * main - Main function
- *
- * Return: Always 0
- */
-int main(void)
-{
-	printf("Main function executed!\n");
-	return (0);
-}
