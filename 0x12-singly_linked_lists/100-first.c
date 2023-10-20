c46390101c219c82bbbf26dd4d38bdb5568e0a95
Haythem <heythem.94@gmail.com>
2023-10-20 01:47:03 +0000

    fixing errors 2-add_node.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,4 +1,4 @@
-#include "list.h"
+#include "lists.h"
 
 /**
  * pre_main_function - Function that prints
--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,25 +3,25 @@
 /**
  * add_node - adds a node to the start of the list
  * @head: address of pointer to head node
- * @s: s field of node
+ * @str: str field of node
  *
  * Return: size of list
  */
-list_t *add_node(list_t **head, const char *s)
+list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_head = malloc(sizeof(list_t));
 
 	if (!head || !new_head)
 		return (NULL);
-	if (s)
+	if (str)
 	{
-		new_head->str = strdup(s);
-		if (!new_head->s)
+		new_head->str = strdup(str);
+		if (!new_head->str)
 		{
 			free(new_head);
 			return (NULL);
 		}
-		new_head->len = _strlen(new_head->s);
+		new_head->len = _strlen(new_head->str);
 	}
 
 	new_head->next = *head;
